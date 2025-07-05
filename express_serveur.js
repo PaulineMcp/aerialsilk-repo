72a89506fe34361e02f5b61e0dc3fce33a419f88
paulinep <pauline.paillassa@gmail.com>
2025-07-05 20:33:10 +0200

    "mise à jour"

--- express_serveur.js
+++ express_serveur.js
@@ -1,51 +1,100 @@
-// pour commencer j'ai créé un projet node dans mon dossier
+// Création du projet Node dans le dossier de travail
 const http_module = require('http') // pour créer un serveur http
-// diff avec require et import ??
-// on utilise const var ??
-// une auter façon de faire serait pour importer uniquement la fonction create server npconst { createServer } = require('node:http'); pour importer seulement la fonction
-//const { createServer } = require('node:http');
-//const server = http_module.createServer( // on devrait initialiser req si on voulait que ça réponde à une requete particulière
-//    (req,res,next) => {res.statusCode = 200,
-//                  res.end('hello lulu') // quelle est la diff avec res.end ??
-//    }
-//); // et ça par exemple, ce n'est pas déjà du routage ???
-// si on ne passait pas par express, il faudrait implémenter manuellement les requêtes get, post, delete etc...
+const fs = require('fs');
+const paparse_module = require('papaparse');
 
-//server.listen(8080); // je lui indique sur quel port écouter
+// Création du serveur avec Express (Express va appeler le serveur)
+const express = require('express');
+const app = express(); // Création d'un objet express
 
-// DEFINIR LES ROUTES ET LES MIDDLESWARES
-
-// on installe express dans le npm : est-ce que ça s'écrit automatiquement dans package.json
-// kezako le fichier package-lock ???
-const express = require('express');// pourquoi pour lancer node ou nodemon je n'ai pas besoin de lui indiquer l'extension du fichier .js
-const app = express(); // création d'un objet express // je ne comprends pas pourquoi il faut forcément l'appeler epxress et app
-// pourquoi const ??
-// app.METHOD(PATH, HANDLER)
-//const port = 3000; est-ce que je peux faire ça en substitution
-app.listen(8080,()=>{
-    console.log('serveur lancé sur 8080');
+// Indiquer un port spécifique sur lequel le serveur doit écouter (ici on a déjà indiqué ci-dessus spécifiquement le port donc pas essentiel) et lancer le serveur
+port = 8080
+app.listen(port, () => {
+  console.log(`Serveur en ligne sur http://localhost:${port}`);
 });
 
-app.get('/user',(req, res) => {
-    res.send('vous avez soumis une demande user');
-  });
+
+// ROUTES
+
+// Route test pour voir que le serveur marche bien
+app.get('/test', (req,res) => {
+    res.send('vous avez fait un test');
+});
+
+// Route avec mot-clé dans l'URL
+app.get('/search',(req, res) => {
+    const user_search = req.query.fig_search_by_user;
+    res.send('vous avez cherché : '.concat(" ",user_search));
+});
+// Cette route a terme doit permettre de récupérer du contenu rentré dans la page HTML par l'utilisateur et dont le contenu est envoyé dans l'URL via une méthode GET par exemple "http://localhost:8080/search?fig_search_by_user=%22pomme%22"
 
 
-// retour suite à une demande get
-//app.get('/user', (req,res) => {
-//    res.send('vous avez soumis une demande user');
-//}); // je comprends pas : est-ce que ça signifie que si le serveur reçoit cette requete 'get /' il renverra 
+// Route pour afficher la base de données
+app.get('/database',(req, res) => {
+ const data = paparse_module.parse(fs.readFileSync('database.csv', 'utf8'), { header: true }).data;
+  res.send(data);
+ });
 
-// pourquoi l'exo d'openclassroom passe par la creation d'un objet server qui a l'air d'être un objet node, pas un objet express
-// attention à ne pas tout confondre, ici / est plutot le sous entendu de la requete, pas de la navigation d'un fichier à un autre
 
-// // servir des fichiers statiques ?? : Servir les fichiers HTML/CSS/JS statiques pour que la page s’affiche dans le navigateur
-// les fichiers statiques sotn des fichiers qui ne changent pas côté client, ex : html, css js client, images etc...le serveur doit fournir ces ficheirs au navigateur
+// Servir les fichiers statiques
+// Importation de la lib Path
 const path = require('path');
-app.use(express.static(path.join(__dirname, ''))); // donne l'accès à des fichiers statiques : le html, le téléchargemtn du csv
-// à long terme il faut donner les bonnes autorisations 
-// déchiffrer cette fonction
+// Mise à dispo des fichiers statiques : ici ils sont mis à dispo dans la root du serveur sauf que 
+app.use(express.static(path.join(__dirname,'')));
+// servir les fichiers statiques
 
-//app.listen(port, () => {
-//  console.log(`Serveur en ligne sur http://localhost:${port}`);
-//});
+
+// Route qui retourne le résultat iSsu de la base de données en fonction de la recherche du user
+app.get('/search_user',(req,res) =>{
+  // récupération du mot de l'utilisateur
+  var user_search = req.query.fig_search_by_user;
+  user_search = user_search.replaceAll('"', '');
+  console.log(user_search);
+  // récupération de la base de données
+  const data = paparse_module.parse(fs.readFileSync('database.csv', 'utf8'), { header: true }).data;
+
+  // Filtre sur la base de données
+  const filtered_results = data.filter(figure => figure.name_fig == user_search);
+  res.send(filtered_results);
+});
+// URL test : http://localhost:8080/search_user?fig_search_by_user=%22radiance%22 => fonctionne, renvoie bien les attributs de radiance
+
+
+// Passage de la base de données en SQL pour pouvoir requêter dessus
+
+// On appelle le SQL
+const mysql = require('mysql2');
+// Création du connecteur
+const connection = mysql.createConnection({
+host: 'localhost',
+user: 'root',
+database: 'aerialsilk',
+password: 'Inna2025'
+});
+
+// Créons une nouvelle route spécialement pour le test du sql
+app.get('/sql_test',(req,res) => {
+  res.send('page test du sql')
+});
+
+// Nouvelle route qui requête en SQL sur la base en SQL
+app.get('/sql_request',(req,res) => {
+  connection.query('SELECT * FROM figures',(err, results) => {
+    res.send(results); }); 
+});
+
+// Route avec un critère 
+app.get('/sql_user_request_criteria',(req,res) => {
+  var user_search = req.query.fig_searched_by_user;
+  //user_search = user_search.replaceAll('"', '');
+  console.log(user_search);
+  sql_raw_request = 'SELECT * FROM figures WHERE name_fig="';
+  sql_request = sql_raw_request.concat("",user_search).concat("",'"');
+  console.log(sql_request);
+
+  connection.query(sql_request,(err, results) => {
+  res.send(results); }); 
+});
+
+// URL test : http://localhost:8080/sql_user_request_criteria?fig_search_by_user=%22cupidon%22
+
